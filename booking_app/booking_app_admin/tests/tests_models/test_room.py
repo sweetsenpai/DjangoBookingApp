cc63646a050e0eaea99b84b0cd1bfdc3d60d16fa
Sweet_Sempai <chcolatemilk00@gmail.com>
2025-04-21 18:12:33 +0300

    Добавил два базовых теста для модели Room

---
+++ booking_app/booking_app_admin/tests/tests_models/test_room.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+from booking_app_admin.models import Room
+
+
+class RoomModelTest(TestCase):
+    def test_create_room(self):
+        room = Room.objects.create(name="101", price_per_day=100.0, capacity=2)
+        self.assertEqual(room.name, "101")
+        self.assertEqual(room.price_per_day, 100.0)
+        self.assertEqual(room.capacity, 2)
+
+    def test_room_capacity_cannot_be_less_than_one(self):
+        with self.assertRaises(ValidationError):
+            room = Room(name="102", price_per_day=100.0, capacity=0)
+            room.full_clean()
