1ce5100dd9ac9aa531e764b865f4d932d99d5ce5
Sweet_Sempai <chcolatemilk00@gmail.com>
2025-04-21 17:59:52 +0300

    Добавил конфиг для pytest и отдельный файл настроек для прохождения тестов.

---
+++ booking_app/booking_app/settings_test.py
@@ -0,0 +1,17 @@
+from .settings_dev import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",  # База данных в памяти для быстрого тестирования
+    }
+}
+
+# Отключить отправку email
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+
+# Включить дебаг, если нужно
+DEBUG = True
+
+# Дополнительные настройки для тестирования
+CACHE_BACKEND = "django.core.cache.backends.locmem.LocMemCache"
