d44328ad69f363469b215c979163efe6ab3fad5f
Sweet_Sempai <chcolatemilk00@gmail.com>
2025-04-21 16:17:04 +0300

    Подготовил проект к работе - созданы два приложения, основное и для api - установленны базовые библиотеки - написан docker-compose для prod и dev версии

---
+++ booking_app/booking_app_admin/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BookingAppAdminConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'booking_app_admin'
---
+++ booking_app/booking_app_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BookingAppApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'booking_app_api'
