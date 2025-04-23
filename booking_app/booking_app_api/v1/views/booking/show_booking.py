286733515fa2b60e576077567bb3b39c5e005dfd
Sweet_Sempai <chcolatemilk00@gmail.com>
2025-04-23 16:29:23 +0300

    Отредактировал название класса отвечающего за эндпоинт выдачи броней

--- booking_app/booking_app_api/v1/views/booking/show_booking.py
+++ booking_app/booking_app_api/v1/views/booking/show_booking.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from drf_spectacular.utils import OpenApiExample, OpenApiParameter, extend_schema
 
@@ -6,6 +7,10 @@ from booking_app_admin.models import Booking
 from booking_app_api.v1.serializers import BookingSerializer
 
 
-class ShowBookingApi(ListAPIView):
+class UserAllBookingApi(ListAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all().prefetch_related("room")
     serializer_class = BookingSerializer
+
+    def get_queryset(self):
+        return Booking.objects.filter(user=self.request.user).prefetch_related("room")
