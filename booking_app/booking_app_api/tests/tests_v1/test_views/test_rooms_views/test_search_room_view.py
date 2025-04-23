6a82ee60e8b7d6e5bc6b798fb807d9e9db922185
Sweet_Sempai <chcolatemilk00@gmail.com>
2025-04-23 19:14:59 +0300

    переработал структуру папки тестов добавил тесты для поиска свободных комнат

---
+++ booking_app/booking_app_api/tests/tests_v1/test_views/test_rooms_views/test_search_room_view.py
@@ -0,0 +1,72 @@
+import pytest
+from django.urls import reverse
+from booking_app_admin.models import Room, Booking
+from datetime import datetime
+from decimal import Decimal
+from django.utils.timezone import make_aware
+
+
+@pytest.fixture
+def test_rooms(db):
+    room1 = Room.objects.create(name="Для двоих", price_per_day=Decimal("100.00"), capacity=2)
+    room2 = Room.objects.create(name="Для одного", price_per_day=Decimal("50.00"), capacity=1)
+    return room1, room2
+
+
+@pytest.fixture
+def test_bookings(test_rooms, django_user_model):
+    room1, room2 = test_rooms
+    superuser = django_user_model.objects.create_superuser(username="admin", password="admin")
+    user = django_user_model.objects.create_user(username="user", password="54321")
+
+    Booking.objects.create(
+        room=room1,
+        user=superuser,
+        date_start=make_aware(datetime(2021, 1, 1)),
+        date_end=make_aware(datetime(2021, 1, 2)),
+    )
+    Booking.objects.create(
+        room=room2,
+        user=user,
+        date_start=make_aware(datetime(2021, 1, 3)),
+        date_end=make_aware(datetime(2021, 1, 4)),
+    )
+    return superuser, user
+
+
+@pytest.mark.django_db
+def test_free_rooms_available(client, test_bookings, test_rooms):
+    url = reverse("search-free-rooms")
+    response = client.get(url, {
+        "date_start": "2026-01-02",
+        "date_end": "2026-01-03",
+        "capacity": 1,
+    })
+
+    assert response.status_code == 200
+    ids = [r["id"] for r in response.data]
+    assert test_rooms[0].id in ids
+    assert test_rooms[1].id in ids
+
+
+@pytest.mark.django_db
+def test_free_rooms_capacity_filter(client, test_bookings, test_rooms):
+    url = reverse("search-free-rooms")
+    response = client.get(url, {
+        "date_start": "2026-01-02",
+        "date_end": "2026-01-03",
+        "capacity": 2,
+    })
+    assert response.status_code == 200
+    ids = [r["id"] for r in response.data]
+    assert test_rooms[0].id in ids
+    assert test_rooms[1].id not in ids
+
+
+@pytest.mark.django_db
+def test_free_rooms_validation_error(client):
+    url = reverse("search-free-rooms")
+    response = client.get(url, {
+        "date_end": "2021-01-03"
+    })
+    assert response.status_code == 400
--- booking_app/booking_app_api/v1/urls.py
+++ booking_app/booking_app_api/v1/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("all-rooms/", ShowRoomsApi.as_view(), name="all-rooms"),
     path("user/booking/", UserAllBookingApi.as_view(), name="user-all-booking"),
     path("user/booking/<int:pk>/", UserBookingApi.as_view(), name="user-booking"),
-    path("free-rooms/", SearchFreeRoomApi.as_view(), name="free-rooms"),
+    path("search-free-rooms/", SearchFreeRoomApi.as_view(), name="search-free-rooms"),
 ]
