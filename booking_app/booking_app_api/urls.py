dea9fc408ab81aa092449e89939e4fa3c2e7b2f7
Sweet_Sempai <chcolatemilk00@gmail.com>
2025-04-24 16:17:11 +0300

    Перенес ссылки на документацию в urls api, так правильнее с архитектурной точки зрения.

--- booking_app/booking_app_api/urls.py
+++ booking_app/booking_app_api/urls.py
@@ -1,5 +1,18 @@
 from django.urls import include, path
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 urlpatterns = [
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path("api/v1/", include("booking_app_api.v1.urls")),
 ]
--- booking_app/booking_app_api/utils/filters/__init__.py
+++ booking_app/booking_app_api/utils/filters/__init__.py
@@ -1,3 +1,3 @@
 from .availible_rooms import get_free_rooms
 
-__all__ = ['get_free_rooms']
+__all__ = ["get_free_rooms"]
