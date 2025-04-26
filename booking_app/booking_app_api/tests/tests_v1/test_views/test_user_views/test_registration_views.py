e1a3bf4eed630b224d8c160230b269b138f53504
Sweet_Sempai <chcolatemilk00@gmail.com>
2025-04-26 17:21:47 +0300

    Покрыл тестами регистрацию

---
+++ booking_app/booking_app_api/tests/tests_v1/test_views/test_user_views/test_registration_views.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+from decimal import Decimal
+
+import pytest
+from booking_app_admin.models import Booking, Room
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase
+
+
+@pytest.mark.django_db(transaction=True, reset_sequences=True)
+class RegistrationApiTest(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_valid_registration(self):
+        url = reverse("user-registration")
+
+        data = {
+            "username": "test_user",
+            "password": "test_password_99",
+            "password2": "test_password_99",
+            "email": "test_email@test.com",
+        }
+
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_invalid_registration_unmatching_passwords(self):
+        url = reverse("user-registration")
+
+        data = {
+            "username": "test_user",
+            "password": "test_password_99",
+            "password2": "test_password_100",
+            "email": "test_email@test.com",
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_invalid_registration_username_exists(self):
+        url = reverse("user-registration")
+
+        data = {
+            "username": "test_user",
+            "password": "test_password_99",
+            "password2": "test_password_99",
+        }
+        self.client.post(url, data)
+        error_response = self.client.post(url, data)
+        self.assertEqual(error_response.status_code, status.HTTP_400_BAD_REQUEST)
